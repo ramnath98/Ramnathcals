a4c549471634bb141b474b8d8baafd5f8821de9b
ramnath98 <dongareramnath55@gmail.com>
2025-09-08 23:51:51 -0400

    add by Rushi Developer

--- zzcalcu/src/com/app/Add.java
+++ zzcalcu/src/com/app/Add.java
@@ -22,4 +22,23 @@ public class Add {
 		
 	}
 
+	public void sub()
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter First Number");
+		
+		int n1 = sc.nextInt();
+		
+        System.out.println("Enter Second Number");
+		
+		int n2 = sc.nextInt();
+		
+		int n3 = n1 - n2;
+		
+		System.out.println("sub : "+n3);
+		
+		
+	}
+
 }
